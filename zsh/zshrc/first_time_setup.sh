e89ed1bda0096b822f4dfb6c16aabb333dd134cf
onibrow <seiya.ono12@gmail.com>
2020-05-04 20:43:56 +0000

    add ohmyzsh script install

--- zsh/zshrc/first_time_setup.sh
+++ zsh/zshrc/first_time_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm ~/.zshrc 2> /dev/null
 ln $SCRIPT_DIR/zshrc ~/.zshrc
